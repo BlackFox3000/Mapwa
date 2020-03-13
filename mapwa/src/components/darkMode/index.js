d8ce1d47c98d30ee6c01d688ba07c309d81b605c
BlackFox3000 <flo-ti@hotmail.fr>
2020-03-13 13:59:07 +0100

    Initialisation Mise en place Page Map Mise en place Thème

---
+++ mapwa/src/components/darkMode/index.js
@@ -0,0 +1,24 @@
+// import React, { useState } from 'react';
+//
+// import './index.css'
+//
+// const Room = () => {
+//     const [isDark, setIsDark] = useState(false);
+//
+//     return (
+//         <div className={isDark ? 'dark' : 'light'} >
+//             <span>Mode: {isDark ? ' Sombre ' :' Lumineu '}</span>
+//             <button onClick={() => {setIsDark(prev => !prev)}}>
+//                 {!isDark ? 'Assombrir' : 'Eclaircir'}
+//             </button>
+//         </div>
+//
+//         //ajouter une autre instance (chambre) // ajouter une lumière par chambre // ajouter temp  // bouton + - temperature
+//     );
+// };
+//
+//
+//
+// export default Room;
+
+
